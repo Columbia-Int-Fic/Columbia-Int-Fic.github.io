fb3cb18a40992ecfa27611ff5d9b74178a01d6a0
rishic83 <68185603+rishic83@users.noreply.github.com>
2020-09-01 14:50:16 +0530

    updated sinead-story

--- TESTER.js
+++ TESTER.js
@@ -8,3 +8,33 @@
 	  audio.play();
 	  });
 </script>
+
+<mark>(link-repeat: "external links")[(open-url: "https://www.facebook.com/")]</mark>
+
+var $2 = document.getElementById('2').addEventListener("click", function() {
+    console.log("bg clicker 2 clicked!");
+    window.top.postMessage('bg2', '*');
+});
+
+|p2)[So yesterday was crazy. And I mean CRAY-ZEE. I went to <span class="clickers" id="3">school</span> and everything was pretty normal. Well as normal as things could be. <span class= "clickers"  id="4">Mam and Dad</span> had told me that a school nearby had closed that week because the virus spread in it. They said I should be careful and not hug my friends or do my <span class= "clickers"  id="5">handshakes</span> with them in case someone had it. It was really awkward when Niamh came up to me to do our |txef>[handshake] and I had to step back and say I didn't want to. She asked me 'why?' So I just said I wasn't in the mood. I was afraid if I told her what Mam and Dad said she'd think that I thought she had the virus, which I don't. But it's hard to know isn’t it? I heard that you can carry the virus but not be coughing or have a high temperature. Also in case she'd make fun of me or <span class= "clickers"  id="6">cough</span> in my face - some kids have done that in my class, and sometimes Niamh can copy others. It was fine though, I think she forgot about the handshake when we went to sit down.
+
+(click-replace: ?txef)[(t8n: "rumble")[handshake]]
+
+  <script>
+	  var $3 = document.getElementById('3').addEventListener("click", function() {
+		  console.log("img clicker 3 clicked!");
+		  window.top.postMessage('img3', '*');	
+	  });
+	  var $4 = document.getElementById('4').addEventListener("click", function() {
+		  console.log("img clicker 4 clicked!");
+		  window.top.postMessage('img4', '*');
+	  });
+	  var $5 = document.getElementById('5').addEventListener("click", function() {
+		  console.log("img clicker 5 clicked!");
+		  window.top.postMessage('img5', '*');
+	  });
+	  var $6 = document.getElementById('6').addEventListener("click", function() {
+		  console.log("img clicker 6 clicked!");
+		  window.top.postMessage('img6', '*');
+	  });
+  </script>]
